85257b3bdcab3685f426626923dedd65cbb4f73b
tuhinaww <iceybubblegumtea@gmail.com>
2023-10-27 22:14:57 +0530

    readbility

--- frontend/public/js/chat.js
+++ frontend/public/js/chat.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const socket = io(); // Initialize a socket connection to the server
-  
-    const messageForm = document.querySelector(".p2 input");
-    const submitButton = document.querySelector(".p3 .submit");
-    const messageList = document.querySelector(".div3");
-     // Handle sending messages
-     submitButton.addEventListener("click", () => {
-      const message = messageForm.value.trim();
-      if (message !== "") {
-        // Emit the message to the server
-        socket.emit("chat message", message);
-  
-        // Clear the input field
-        messageForm.value = "";
-      }
-    });
-     // Handle receiving messages
-     socket.on("chat message", (message) => {
-      const messageElement = document.createElement("div");
-      messageElement.textContent = message;
-      messageList.appendChild(messageElement);
-    });  
+  const socket = io(); // Initialize a socket connection to the server
 
+  const messageForm = document.querySelector(".p2 input");
+  const submitButton = document.querySelector(".p3 .submit");
+  const messageList = document.querySelector(".div3");
 
+  // Handle sending messages
+  submitButton.addEventListener("click", () => {
+    const message = messageForm.value.trim();
+    if (message !== "") {
+      // Emit the message to the server
+      socket.emit("chat message", message);
+
+      // Clear the input field
+      messageForm.value = "";
+    }
   });
-  
+
+  // Handle receiving messages
+  socket.on("chat message", (message) => {
+    const messageElement = document.createElement("div");
+    messageElement.textContent = message;
+    messageList.appendChild(messageElement);
+  });
+});

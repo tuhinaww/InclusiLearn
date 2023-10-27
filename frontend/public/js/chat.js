6a3e66ffcfecf721dd3c5ab740de248ea7a02fa1
tuhinaww <iceybubblegumtea@gmail.com>
2023-10-27 22:13:13 +0530

    js for chat section

---
+++ frontend/public/js/chat.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const socket = io(); // Initialize a socket connection to the server
+  
+    const messageForm = document.querySelector(".p2 input");
+    const submitButton = document.querySelector(".p3 .submit");
+    const messageList = document.querySelector(".div3");
+  
+
+  });
+  

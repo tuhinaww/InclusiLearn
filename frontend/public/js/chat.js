8dc2719d7373e6cc992c50e76f68a425b332cf9f
tuhinaww <iceybubblegumtea@gmail.com>
2023-10-27 22:14:07 +0530

    sending message

--- frontend/public/js/chat.js
+++ frontend/public/js/chat.js
@@ -4,7 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const messageForm = document.querySelector(".p2 input");
     const submitButton = document.querySelector(".p3 .submit");
     const messageList = document.querySelector(".div3");
+     // Handle sending messages
+     submitButton.addEventListener("click", () => {
+      const message = messageForm.value.trim();
+      if (message !== "") {
+        // Emit the message to the server
+        socket.emit("chat message", message);
   
+        // Clear the input field
+        messageForm.value = "";
+      }
+    });
+  
+
 
   });
   

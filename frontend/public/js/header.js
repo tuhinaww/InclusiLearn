663fb435abaf0732d6f9bc118548a389444c6d41
tuhinaww <iceybubblegumtea@gmail.com>
2023-10-27 23:19:02 +0530

    header file

---
+++ frontend/public/js/header.js
@@ -0,0 +1,11 @@
+let logo = document.querySelector(".project-header")
+let home = document.querySelector(".option3-header")
+let about = document.querySelector(".option1-header")
+let chats = document.querySelector(".option2-header")
+let profile = document.querySelector(".name-header");
+let search_header = document.querySelector(".option4-header");
+
+logo.addEventListener("click",function(){
+    window.location.href = "/homepage"
+})
+

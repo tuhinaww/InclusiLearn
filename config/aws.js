c1fa12c5022b77b7d9c7eef15bdfe040bbcd0297
sadaf <sadaf20043006@gmail.com>
2023-10-28 19:30:02 +0530

    added: aws config

---
+++ config/aws.js
@@ -0,0 +1,20 @@
+const AWS = require('aws-sdk');
+const config = new AWS.Config({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+AWS.config.update(config);
+
+AWS.config.getCredentials(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to AWS successfully');
+  }
+});
+
+exports.S3 = new AWS.S3();
+
+exports.AWS;

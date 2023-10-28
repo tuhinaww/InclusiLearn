fe0f384a5789204d0d2d2af09885a4e336642066
sadaf <sadaf20043006@gmail.com>
2023-10-28 19:33:21 +0530

    added: user service

---
+++ services/UserService.js
@@ -0,0 +1,13 @@
+const User = require('../models/UserModel');
+
+exports.getUsername = async (userId) => {
+    try {
+        const user = await User.findOne({ _id: userId });
+        if (!user) {
+            console.log('user does not exist');
+        }
+        return user.username;
+    } catch (err) {
+        console.error(err);
+    }
+};

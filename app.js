07219d432e20c7882ff96c339ea5c49371aa5dac
sadaf <sadaf20043006@gmail.com>
2023-10-28 15:31:31 +0530

    added: connection to mongo

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 
 const port = 5000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 // Navigation
 

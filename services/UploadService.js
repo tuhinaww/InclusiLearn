f8d910c1a48c0c443ac5a13e852ee632c09bc09b
sadaf <sadaf20043006@gmail.com>
2023-10-28 19:32:49 +0530

    added: upload service

---
+++ services/UploadService.js
@@ -0,0 +1,18 @@
+const UploadPost = require('../models/UploadPostModel');
+
+exports.uploadPost = async (userId, title, challenge, solution, objectKey) => {
+    try {
+        const newPost = new UploadPost({
+            userId,
+            title,
+            challenge,
+            solution,
+            objectKey,
+        })
+    
+        await newPost.save();
+        console.log('post added successfully')
+        } catch (err) {
+            console.error(err);
+        }
+};
